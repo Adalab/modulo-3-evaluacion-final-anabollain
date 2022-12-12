9760c21cdd51d17a2c9d4a2b957b155292b4c21d
Ana Bollain <ana.bollain@gmail.com>
2022-12-12 16:08:15 +0100

    Add return to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-function Header(props) {
+//libraries
+import { Link } from 'react-router-dom';
+//styles
+//import '../styles/components/Header.scss';
+
+
+function Header() {
 
   return (
-    <></>
+    <header className='header'>
+            <Link to='/' className='header__title'>
+                <h1>Rick And Morty</h1>
+            </Link>
+            <nav>
+                <ul className='header__nav'>
+                    <li><Link to="/" className='header__nav--link'>Home</Link></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
-
-Header.propTypes = {
-
-};
 
 export default Header;
